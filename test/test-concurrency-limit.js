c85e887aa2c46b6cd21be973cd30208d41d42135
alanclarke <alan@qubit.com>
2020-02-26 20:27:31 +0000

    Rename

--- test/test-concurrency-limit.js
+++ test/test-concurrency-limit.js
@@ -1,12 +1,12 @@
 const test = require('ava')
-const concurrencyLimit = require('../')
-const inputErrorMessage = 'concurrency-limit expects an integer greater than zero'
+const limitConcurrency = require('../')
+const inputErrorMessage = 'limit-concurrency expects an integer greater than zero'
 
 ;[undefined, 2, 3, 4, 5].forEach(limit => {
   test(`allows max concurrency of ${limit || 1}`, async t => {
     let active = 0
     const activeChecks = []
-    const check = concurrencyLimit(async () => {
+    const check = limitConcurrency(async () => {
       active++
       await new Promise(resolve => setTimeout(resolve, 1))
       activeChecks.push(active)
@@ -21,27 +21,27 @@ const inputErrorMessage = 'concurrency-limit expects an integer greater than zer
 })
 
 test('accepts and returns args', async t => {
-  const stub = concurrencyLimit(args => args)
+  const stub = limitConcurrency(args => args)
   const args = [1, 2, 3]
   t.is(await stub(args), args)
 })
 
 test('throws errors', async t => {
   const eek = new Error('eek')
-  const stub = concurrencyLimit(() => {
+  const stub = limitConcurrency(() => {
     throw eek
   })
   return stub().catch(err => t.is(err, eek))
 })
 
 test('rejects n less than zero', async t => {
-  t.throws(() => concurrencyLimit(() => {}, -1), {
+  t.throws(() => limitConcurrency(null, -1), {
     message: inputErrorMessage
   })
 })
 
 test('rejects non-integer n', async t => {
-  t.throws(() => concurrencyLimit(() => {}, NaN), {
+  t.throws(() => limitConcurrency(null, NaN), {
     message: inputErrorMessage
   })
 })
